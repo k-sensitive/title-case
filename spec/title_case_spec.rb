fb46c236760f638907bf56bdf3af1c660d4fb654
Courtney Phillips & Kim Dang <student@epicodus.com>
2015-04-22 09:34:39 -0700

    Add integration specs.

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -3,14 +3,14 @@ require('./lib/title_case')
 
 describe('String#title_case') do
   it("capitalizes the first letter of a word") do
-    expect (("beowulf").title_case()).to(eq("Beowulf"))
+    expect(("beowulf").title_case()).to(eq("Beowulf"))
   end
 
   it("capitalizes multiple word titles") do
-    expect(("beowulf begins").title_case().to(eq("Beowulf Begins"))
+    expect(("beowulf begins").title_case()).to(eq("Beowulf Begins"))
   end
 
-  it("does not capitalize designated wors that are after the first word") do
+  it("does not capitalize designated words that are after the first word") do
     expect(("from beowulf to the incredible hulk").title_case()).to(eq("From Beowulf to the Incredible Hulk"))
   end
 
